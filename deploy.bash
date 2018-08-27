1cb1997b46198e2eac7c27798726fb06ef1535f8
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2018-08-27 15:26:35 +0200

    Initial work on a deployment script.

---
+++ deploy.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Deploy the packages and website.
+
+#echo '[]' > html/download_counts.json
+#echo '{}' > html/build-status.json
+#gpg --detach-sign --armor -o html/packages/archive-contents.sig html/packages/archive-contents
+#rsync --archive --copy-links --verbose --recursive html/* hsrv:/var/www/gustafwaldemarson.com/elpa
