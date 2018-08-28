de53cbdad4cc68609c1be81baced90a22540a341
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2018-08-28 16:40:06 +0200

    Deployment script working.
    - Might want to separate some of the steps however.
    
    - Also, the generated webpage sure needs some work.

--- deploy.bash
+++ deploy.bash
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # Deploy the packages and website.
 
-#echo '[]' > html/download_counts.json
-#echo '{}' > html/build-status.json
-#gpg --detach-sign --armor -o html/packages/archive-contents.sig html/packages/archive-contents
-#rsync --archive --copy-links --verbose --recursive html/* hsrv:/var/www/gustafwaldemarson.com/elpa
+echo '[]' > html/download_counts.json
+echo '{}' > html/build-status.json
+for pkg in html/packages/archive-contents html/packages/*.tar;
+do
+    gpg --detach-sign --armor -o $pkg.sig $pkg
+done
+rsync --archive --copy-links --verbose --recursive html/* hsrv:/var/www/gustafwaldemarson.com/elpa
